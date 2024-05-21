0b81366d65888f2216e01780623e05f22d1c5b03
paulahakeem <paula.hakeem@yahoo.com>
2024-05-22 01:33:23 +0300

    uploading files

---
+++ main.tf
@@ -0,0 +1,127 @@
+module "network" {
+  source               = "./modules/NETWORK"
+  vpc_name             = "Paula-MainVpc"
+  vpc_cidr             = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  subnets                     = var.subnets
+  availability_zones          = "us-east-1a"
+  auto_assign_public_ip_state = true
+  cidr_from_anywhere          = "0.0.0.0/0"
+  public_route_name           = "public-route"
+  private_route_name          = "private-route"
+  internet_gateway_name       = "paula-IGW"
+  natgateway_name             = "paula-NGW"
+  security_group_name         = "paula-SG"
+  security_group_description  = "Allow HTTP traffic from anywhere"
+  inport                      = ["80", "22", "3306", "9000", "3001"]
+  in_protocol                 = "tcp"
+  eg_port                     = 0
+  eg_protocol                 = "-1"
+}
+
+module "backendEC2" {
+  source                      = "./modules/VMS"
+  ec2_name                    = "paula-db"
+  ec2_ami                     = "ami-04868818694d21658"
+  ec2_type                    = "t3.micro"
+  SG_id                       = [module.network.secgroup-id]
+  ec2_subnet_ID               = module.network.private_subnet_id1
+  associate_public_ip_address = false
+  key_pair                    = "paula-key"
+  user_data                   = file("./install2.sh")
+  depends_on                  = [module.network]
+}
+
+resource "null_resource" "update_docker_compose" {
+  provisioner "local-exec" {
+    command = "bash ./update_docker_compose.sh"
+  }
+  depends_on = [module.backendEC2]
+}
+
+module "load_balancer" {
+  source                     = "./modules/LB"
+  lb_name                    = "paulaLB"
+  internal                   = false
+  load_balancer_type         = "application"
+  security_group_ids_LB      = [module.network.secgroup-id]
+  subnet_ids_LB              = [module.network.public_subnet_id1, module.network.public_subnet_id2]
+  enable_deletion_protection = false
+  port                       = 80
+  protocol                   = "HTTP"
+  vpc_id                     = module.network.vpc_id
+  depends_on                 = [null_resource.update_docker_compose]
+}
+
+module "lunch_template" {
+  source                      = "./modules/TEMPLATE"
+  name                        = "paula-terraform-template"
+  ami_id                      = "ami-04b70fa74e45c3917"
+  instance_type               = "t3.micro"
+  key_name                    = "paula-key"
+  security_group_id           = [module.network.secgroup-id]
+  subnet_id                   = module.network.public_subnet_id1
+  associate_public_ip_address = true
+  user_data                   = base64encode(file("./install.sh"))
+  monitoring                  = true
+  depends_on                  = [null_resource.update_docker_compose]
+}
+
+module "ASG" {
+  source                     = "./modules/ASG"
+  name                       = "paula-asg-5548"
+  health_check_grace_period  = 100
+  health_check_type          = "ELB"
+  force_delete               = true
+  vpc_zone_identifier        = [module.network.public_subnet_id1, module.network.public_subnet_id2]
+  desired_capacity           = 1
+  max_size                   = 4
+  min_size                   = 1
+  launch_template_id         = module.lunch_template.launch_template_id
+  versions                   = "$Latest"
+  cpu_policy_percentage      = 50.0
+  ec2_instance_name          = "paula"
+  alb_target_group_arn       = module.load_balancer.target_group_arn
+  depends_on                 = [null_resource.update_docker_compose]
+}
+
+
+
+
+
+output "mysql_private_ip" {
+  value = module.load_balancer.lb_dns_name
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # ----------------------- ------------------------------------#
+# module "frontendEC2" {
+#   source                      = "./modules/VMS"
+#   ec2_name                    = "frontend"
+#   ec2_ami                     = "ami-0cd59ecaf368e5ccf"
+#   ec2_type                    = "t2.micro"
+#   SG_id                       = [module.network.secgroup-id]
+#   ec2_subnet_ID               = module.network.public_subnet_id
+#   associate_public_ip_address = true
+#   key_pair                    = "paula"
+#   user_data                   = file("./install.sh")
+# }
+# # ----------------------- ------------------------------------#
---
+++ provider.tf
@@ -0,0 +1,15 @@
+/* Configure the AWS Provider */
+provider "aws" {
+  access_key = var.access_key
+  region     = "us-east-1"
+  secret_key = var.secret_key
+}
+
+
+
+# /* Configure the AWS Provider */
+# provider "aws" {
+#   access_key = "AKIAZQ3DQK2FH6EIM6X3"
+#   region     = "us-east-1"
+#   secret_key = "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"
+# }
